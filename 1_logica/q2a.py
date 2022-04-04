f1a827982f5d0b923afda14d09436b378e460efb
Breno Sousa <brenosousa@usp.br>
2022-04-03 23:10:38 -0300

    comments

--- 1_logica/q2a.py
+++ 1_logica/q2a.py
@@ -2,12 +2,12 @@ def estacionamento_ok(K, instantes):
     s = []
     resultado = "nao"
     for i in range(0, len(instantes)):
-        if instantes[i] > 0:
+        if instantes[i] > 0: # Se a entrada for > 0 adiciona-a ao estacionamento
             s.append(instantes[i])
-            if (len(s)) > K:
+            if (len(s)) > K: # Checa se o estacionamento lotou
                 return "nao"
 
-        if instantes[i] < 0 and (instantes[i] * -1) == s[-1]:
+        if instantes[i] < 0 and (instantes[i] * -1) == s[-1]: # Caso a entrada seja negativa e válida, remove o carro
             s.pop()
 
         if len(s) == 0:
